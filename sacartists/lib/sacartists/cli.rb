c032bb6771a6150dcad883aaa0d8e44afe176cb3
Sara A <calisaraalhindi@gmail.com>
2020-09-19 20:18:23 +0000

    part2

--- sacartists/lib/sacartists/cli.rb
+++ sacartists/lib/sacartists/cli.rb
@@ -4,6 +4,11 @@ module SacArtists
   def call 
     puts 'Welcome to the Sacramento Artists app!'
     # binding.pry
+    # get_artist
+    # get_careers
+    # get_artists_for(career)
+    #  list artists 
+    #  user chooses artist 
   end
 end
 end
