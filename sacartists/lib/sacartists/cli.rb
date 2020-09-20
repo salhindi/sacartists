9a35011ae5756f5db66519b8b473a1acacd3dd5c
Sara A <calisaraalhindi@gmail.com>
2020-09-20 00:51:31 +0000

    validinput

--- sacartists/lib/sacartists/cli.rb
+++ sacartists/lib/sacartists/cli.rb
@@ -17,21 +17,21 @@ class Sacartists::CLI
   end
   
   def list_careers
-     @careers.each_with_index(1) do |c, i|
-       puts "#{i}.#{c}"
+     @careers.each_with_index do |c, i|
+       puts "#{i +1}.#{c}"
      end
    end
      
     def get_user_careers
       chosen_career = gets.strip
       binding.pry
-      # if valid_input(chosen_career)
+      # if valid_input(chosen_career, @careers)
       #   puts "true"
     end
     
-    
     def valid_input(input, data)
-      input.to_i <= data.length && input.to_i < 0 
+      input.to_i <= data.length && input.to_i > 0 
     end
+  
     
 end
