fb263c8d73de9a8259b75cb89e56c59d3312e70a
Sara A <calisaraalhindi@gmail.com>
2020-09-20 06:49:51 +0000

    menu

--- sacartists/lib/sacartists/cli.rb
+++ sacartists/lib/sacartists/cli.rb
@@ -2,41 +2,46 @@ require 'pry'
 class Sacartists::CLI
     attr_accessor :name
     
-  def call 
-    puts 'Welcome to the Sacramento Artists app!'
-    get_career_options
-    list_careers
-    get_career_choice
+   def call 
+      puts 'Welcome to the Sacramento Artists app!'
+      get_career_options
+      
+      list_careers
+      puts "Type in the number of the talent you're looking for."
+      get_career_choice
+      
+      puts "To leave the app, type-in 'exit'"
+    end
   end
+
   
-  def get_career_options
-    @careers = Sacartists::Career.all
-  end
+    def get_career_options
+      @careers = Sacartists::Career.all
+    end
   
-  def list_careers
-    puts "Choose a talent."
-     @careers.each_with_index do |c, i|
-       puts "#{i +1}.#{c.name}"
-     end
-   end
+    def list_careers
+      puts "What talent are you looking for?"
+      @careers.each_with_index do |c, i|
+      puts "#{i +1}.#{c.name}"
+    end
      
     def get_career_choice
       chosen_career = gets.strip.to_i
       show_artists_for(chosen_career) if valid_input(chosen_career, @careers)
     end
+  end
     
     def valid_input(input, data)
       input.to_i <= data.length && input.to_i > 0 
     end
+  
     
     def show_artists_for(chosen_career)
       career = @careers[chosen_career -1]
       career.get_artists 
       puts "Here is a list of artists that are #{career.name}."
-     career.artists.each_with_index do |artist, i|
+      career.artists.each_with_index do |artist, i|
        puts "#{i +1}. #{artist.name}" 
      end
-    end
-  
-    
-end
+   end
+ 
--- sacartists/lib/sacartists/scraper.rb
+++ sacartists/lib/sacartists/scraper.rb
@@ -1,16 +1,23 @@
 class Sacartists::Scraper
-  def self.scrape_careers
-    doc = Nokogiri::HTML(open("https://www.sacramento365.com/artist/?view&onepage=1&type"))
-    careers = doc.css("select#artist-type-select option")
+  
+   
+   
+    def self.scrape_careers
+   doc = Nokogiri::HTML(open("https://www.sacramento365.com/artist/?view&onepage=1&type"))
+     careers = doc.css("select#artist-type-select option")
 
-    careers.each do |c|
-      name = c.text
+      careers.each do |c|
+        name = c.text
       Sacartists::Career.new(name)
-    end
+      end
     end
     
     def self.scrape_artists(career)
-      Sacartists::Artist.new("Jaren Fifield", career)
+       doc = Nokogiri::HTML(open("https://www.sacramento365.com/artist/?view&onepage=1&type"))
+      artists = doc.css(".search-info").css(".ev-tt")
+      artists.each do |r| 
+        puts r.css('span.ev-tt').text
+      end
     end
+  end
     
-end
